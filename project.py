0c7932d6c321a14d09cf98d3f1473c3fb0c2d009
Proyarko <kievyaroslav14@gmail.com>
2023-11-24 22:53:10 +0200

    last

---
+++ project.py
@@ -0,0 +1,36 @@
+import sqlite3
+import requests
+from bs4 import BeautifulSoup as bs
+# class Table:
+#     connection = sqlite3.connect('pr.db')
+#     cursor = connection.cursor()
+#     cursor.execute("""CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY,
+#                                         name VARCHAR(100) UNIQUE NOT NULL,
+#                                         link VARCHAR(100) NOT NULL);""")
+    # connection = sqlite3.connect('pr.db')
+    # cursor = connection.cursor()
+    # cursor.execute("""INSERT INTO user (name, link) 
+    #                   VALUES('weather', 'https://ua.sinoptik.ua/?utm_source=ukr.net&utm_medium=main_page&utm_campaign=main_izbrannoe'), 
+    #                   ('value', 'https://www.oschadbank.ua/currency-rate'),
+    #                   ('news','https://www.ukr.net')""")
+    # connection = sqlite3.connect('pr.db')
+    # cursor = connection.cursor()
+    # cursor.execute("""SELECT name FROM user""")
+    # data = cursor.fetchall()
+    # print(data)
+    # connection.commit()
+    # connection.close()
+class Reaq:
+    r = requests.get("https://www.oschadbank.ua/currency-rate")
+    html = bs(r.text, "html.parser")
+    data = html.find_all('span', class_='heading-block-currency-rate__table-txt body-regular')
+    cur = []
+    for i in data:
+        print(i.span.text)
+# class Reaq1:
+#     r = requests.get("https://www.oschadbank.ua/currency-rate")
+#     html = bs(r.text, "html.parser")
+#     data1 = html.find_all('span', class_='heading-block-currency-rate__table-txt body-regular')
+#     cur = []
+#     for i in data1:
+#         print(i.text)
